c34cbf0e3e2d6db4338b7e86d75a8687a0612457
divyatellabati26 <95356480+divyatellabati26@users.noreply.github.com>
2022-11-06 16:43:42 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+import AxiosDemo from "./AxiosDemo";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>H</h1>
+      <AxiosDemo />
+    </div>
+  );
+}
